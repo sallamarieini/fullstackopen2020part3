a1ff11da856c832772eba3d1cf1e1fc979e9f01a
sallamarieini <sallamarieini@gmail.com>
2020-10-11 20:21:42 +0300

    Exercise 3.3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -33,6 +34,17 @@ app.get('/info', (req, res) => {
         `<p> Phonebook has info for ${persons.length} people </p>
          <p> ${Date()} </p>`
     )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
